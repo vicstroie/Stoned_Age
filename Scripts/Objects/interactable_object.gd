df196b304ae53ccf6595e1b5b2bb4d4221cf213a
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-08-17 18:22:53 -0700

    Update interactable_object.gd

--- Scripts/Objects/interactable_object.gd
+++ Scripts/Objects/interactable_object.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (picked_up):
+		visible = false
 		self.set_process(false)
 
 func pick_up() -> InvItem:
